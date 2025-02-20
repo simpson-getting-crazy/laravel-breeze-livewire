0aaf823a05dd2541e6857c8551f5cc327eab6906
dayCod <alayarahman07@gmail.com>
2025-02-20 10:54:16 +0700

    ui nprogress bar color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,12 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <style>
+            #nprogress .bar {
+                background: #DC2626 !important;
+            }
+        </style>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
